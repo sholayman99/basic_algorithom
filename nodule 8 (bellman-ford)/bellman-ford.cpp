8562b325a4e3ba0fd80c62c0ff810e4e914e0195
sholayman99 <mdsholayman998@gmail.com>
2025-09-01 22:23:45 +0600

    learned flyod warshall algorithm

--- nodule 8 (bellman-ford)/bellman-ford.cpp
+++ nodule 8 (bellman-ford)/bellman-ford.cpp
@@ -14,21 +14,11 @@ public:
     }
 };
 int dis[1005];
-int main()
-{
-    int n, e;
-    cin >> n >> e;
-    vector<Edge> edge_list;
-    while (e--)
-    {
-        int a, b, c;
-        cin >> a >> b >> c;
-        edge_list.push_back(Edge(a, b, c));
-    }
+int n, e;
+vector<Edge> edge_list;
 
-    for (int i = 0; i < n; i++)
-        dis[i] = INT_MAX;
-    dis[0] = 0;
+void bellman_ford() // Complexity- O(VE)
+{
 
     for (int i = 0; i < n - 1; i++)
     {
@@ -43,9 +33,47 @@ int main()
                 dis[b] = dis[a] + c;
         }
     }
+    bool cycle = false;
+    for (auto ed : edge_list)
+    {
+        int a, b, c;
+        a = ed.a;
+        b = ed.b;
+        c = ed.c;
+
+        if (dis[a] != INT_MAX && dis[a] + c < dis[b])
+        {
+            cycle = true;
+            break;
+        }
+    }
+    if (cycle)
+        cout << "Negative cycle detected\n";
+    else
+    {
+        for (int i = 0; i < n; i++)
+            cout << i << " -> " << dis[i] << endl;
+    }
+}
+
+int main()
+{
+
+    cin >> n >> e;
+
+    while (e--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        edge_list.push_back(Edge(a, b, c));
+        // edge_list.push_back(Edge(b, a, c)); //--> For unweighted graph only
+    }
 
     for (int i = 0; i < n; i++)
-        cout << i << " -> " << dis[i] << endl;
+        dis[i] = INT_MAX;
+    dis[0] = 0;
+
+    bellman_ford();
 
     return 0;
 }
